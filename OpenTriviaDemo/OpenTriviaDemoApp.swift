b0f0737dfa4ad1a4b4d11b8d8c40a78a3944549b
alextuchkov <alextychkoff@gmail.com>
2024-11-08 16:00:47 +0200

    Initial Commit

---
+++ OpenTriviaDemo/OpenTriviaDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenTriviaDemoApp.swift
+//  OpenTriviaDemo
+//
+//  Created by Oleksandr Tuchkov on 08.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct OpenTriviaDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
